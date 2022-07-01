b730ca5aacb0febc58cafadf1a518c579779bf01
eb <eiler.byberi@gmail.com>
2022-06-30 20:04:24 -0700

    Format the code

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,5 @@
 def stock_picker(stock_prices)
-  buy_sell = stock_prices.combination(2).to_a.sort { |a, b| ((b[1] - b[0]) <=> (a[1]-a[0]))}.first
+  buy_sell = stock_prices.combination(2).to_a.sort { |a, b| ((b[1] - b[0]) <=> (a[1]-a[0])) }.first
   stock_pick = []
   stock_pick << stock_prices.index(buy_sell[0])
   stock_pick << stock_prices.index(buy_sell[1])
