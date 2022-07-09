1bbdc18b1dd48b3ce3cd0958c69af6adcfef2c39
eb <eiler.byberi@gmail.com>
2022-07-08 19:33:35 -0700

    Fix formatting

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,9 +1,9 @@
 def stock_picker(stock_prices)
-  buy_sell = stock_prices.combination(2).to_a.sort { |a, b| ((b[1] - b[0]) <=> (a[1]-a[0])) }.first
+  buy_sell = stock_prices.combination(2).to_a.min { |a, b| ((b[1] - b[0]) <=> (a[1] - a[0])) }
   stock_pick = []
   stock_pick << stock_prices.index(buy_sell[0])
   stock_pick << stock_prices.index(buy_sell[1])
   stock_pick
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
